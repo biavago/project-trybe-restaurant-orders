f61bb7e47679df734a51822c77d33eb1ea173c7c
Bia <beatriz.evago@gmail.com>
2023-10-06 19:48:02 +0100

    Iniciando o projeto

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,8 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    teste = Ingredient('camarão')
+
+    assert teste.__repr__() == "Ingredient('camarão')"
+    assert teste.__hash__() == hash('camarão')
+    

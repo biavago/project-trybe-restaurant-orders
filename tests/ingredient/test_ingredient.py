decd4787a3477947a7965c4503b4e0fac46c7985
Beatriz Vago <beatriz@C02Z88A1LVCF.Home>
2023-10-09 15:31:39 +0100

    Teste

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -5,6 +5,9 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 def test_ingredient():
     teste = Ingredient('camarão')
 
+    assert teste == Ingredient('camarão')
     assert teste.__repr__() == "Ingredient('camarão')"
     assert teste.__hash__() == hash('camarão')
-    
+    assert teste.name == 'camarão'
+    assert teste.restrictions
+    assert teste != 'blablabla'
